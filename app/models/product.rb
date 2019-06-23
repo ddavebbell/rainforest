588bba035031e3cdd4cd99568ac7c37406204a4c
somethinrother <jamesneilson4@gmail.com>
2019-06-23 13:46:36 -0400

    Update price to price_in_cents

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,10 @@
 class Product < ApplicationRecord
 	has_many :reviews
 
-	validates :name, :description, :price, presence: true
+	validates :name, :description, :price_in_cents, presence: true
+	validate :price_in_cents_is_integer
+
+	def price_in_cents_is_integer
+
+	end
 end
