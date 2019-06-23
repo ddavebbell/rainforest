3e3f1fb568477bbf887cef511f92a8554dda33dd
somethinrother <jamesneilson4@gmail.com>
2019-06-23 12:43:42 -0400

    Fix out routing for products and reviews, render links to review index propaly

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-	resources :products
-
- 	resources :reviews do
- 		resources :products
- 	end
+	resources :products do
+		resources :reviews
+	end
 
 	root :to => 'products#index'
 
