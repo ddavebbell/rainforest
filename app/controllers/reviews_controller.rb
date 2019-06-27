33f3efc7145f0259f811834c91e290e83ebf715a
somethinrother <jamesneilson4@gmail.com>
2019-06-27 16:49:01 -0400

    Reviews can be updated properly

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,5 @@
 class ReviewsController < ApplicationController
 before_action :load_review, only: [:new, :edit, :show, :update, :destroy]
-# before_action :ensure_user_owns_review, except: [:index, :show, :new]
 
 	def index
 		@reviews = Product.first.reviews.all
@@ -42,8 +41,17 @@ before_action :load_review, only: [:new, :edit, :show, :update, :destroy]
 	end
 
 	def update
-		redirect_to product_path(params[:product_id])
+		@review.assign_attributes(
+			name: params[:review][:name],
+			text: params[:review][:text],
+			product_id: params[:product_id],
+			user_id: current_user.id )
+
+			if @review.save
+				redirect_to product_path(params[:product_id])
+				flash[:notice] = "Review successfully updated"
+			else
+				flash[:notice] = "Nothing happened."
+			end
 	end
-
-
 end
