65b158e0b11ab45262a929a249165736aa810d28
ddavebbell <daverichardbell@gmail.com>
2019-06-27 19:58:48 -0400

    working on update-review-issue

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -16,16 +16,17 @@ before_action :load_review, only: [:new, :show, :update, :destroy]
 	end
 
 	def edit
-		@review = Review.find_by(product_id: params[:id])
+		reviews = Review.where(product_id: params[:id])
+		@review = reviews.find(params[:product_id])
 	end
 
 	def create
 		@review = Review.new
-		@review.assign_attributes(
+		@review.assign_attributes({
 			name: params[:review][:name],
 			text: params[:review][:text],
 			product_id: params[:product_id],
-			user_id: current_user.id )
+			user_id: current_user.id })
 
 			if @review.save
 				redirect_to product_path(params[:product_id])
@@ -42,17 +43,16 @@ before_action :load_review, only: [:new, :show, :update, :destroy]
 			product_id: params[:product_id],
 			user_id: current_user.id )
 
-		if @review.save
-			redirect_to product_path(params[:product_id])
-			flash[:notice] = "Review successfully updated"
-		else
-			flash[:notice] = "Nothing happened."
-		end
+		# if @review.save
+		# 	redirect_to product_path(params[:product_id])
+		# 	flash[:notice] = "Review successfully updated"
+		# else
+		# 	flash[:notice] = "Nothing happened."
+		# end
+	end
 
 		def destroy
 			@review.destroy
 			redirect_to root_url, notice: "Destruction successful!"
 		end
-	end
-
 end
